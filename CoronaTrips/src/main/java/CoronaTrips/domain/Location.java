36acaf486802d4de092c9feed3a60c7f910a1c95
nicu <nicu>
2022-02-15 12:39:39 +0200

    code refactoring

--- CoronaTrips/src/main/java/CoronaTrips/domain/Location.java
+++ CoronaTrips/src/main/java/CoronaTrips/domain/Location.java
@@ -4,10 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
 
-import java.util.List;
-import java.util.Set;
+
 
 @Data
 @AllArgsConstructor
